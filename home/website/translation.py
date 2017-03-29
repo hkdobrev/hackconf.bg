f1cb6dc2b065917163b97e56dbce73e64550ba07
Slavyana Monkova <slaviana_m@mail.bg>
2017-03-29 15:15:42 +0300

    Adding all needed fields for translation

--- home/website/translation.py
+++ home/website/translation.py
@@ -6,6 +6,14 @@ from wagtail_modeltranslation import patch_wagtailadmin
 
 @register(HomePage)
 class HomePageTranslationOptions(WagtailTranslationOptions):
-    fields = ('slug', 'url_path', 'live', 'title', 'header_text', 'about_text', 'call_for_speakers_title')
+    fields = ('title', 'header_text', 'about_text', 'call_for_speakers_title', 'call_for_speakers_description',
+            #sponsors section
+            'sponsors_text', 'sponsors_partnership_description',
+            #tickets section
+            'tickets_title', 'tickets_description',
+            #location section
+            'location_title', 'location_place',
+            #fields from Page model
+            'slug', 'url_path', 'live')
 
     required_languages = ("bg", "en")
