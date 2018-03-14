90a9953bda3d16eca430a7001ee911e83fb0b4b3
Kamen Kotsev <kkotsev94@gmail.com>
2018-03-14 15:24:49 +0200

    Add translation for pre-register section

--- home/website/translation.py
+++ home/website/translation.py
@@ -2,23 +2,40 @@ from .models import HomePage, Event, NavigationItem, Speaker, Lecture
 from modeltranslation.decorators import register
 from wagtail_modeltranslation.translator import WagtailTranslationOptions
 
-from wagtail_modeltranslation import patch_wagtailadmin
 
 @register(HomePage)
 class HomePageTranslationOptions(WagtailTranslationOptions):
-    fields = ('title', 'header_dates', 'header_text', 'about_text', 'call_for_speakers_title', 'call_for_speakers_description',
-            #partners section
-            'gold_partners_title', 'silver_partners_title', 'platinum_partners_title',
-            'general_partners_title', 'other_partners_title', 'media_partners_title',
-            'branch_partners_title',
-            'sponsors_text', 'sponsors_partnership_description',
-            #tickets section
-            'tickets_title', 'tickets_description',
-            #location section
-            'location_title', 'location_place',
-            #fields from Page model
-            'slug', 'url_path', 'live',
-            'speakers_title', 'schedule_title')
+    fields = (
+        'title',
+        'header_dates',
+        'header_text',
+        'about_text',
+        'call_for_speakers_title',
+        'call_for_speakers_description',
+        # partners section
+        'gold_partners_title',
+        'silver_partners_title',
+        'platinum_partners_title',
+        'general_partners_title',
+        'other_partners_title',
+        'media_partners_title',
+        'branch_partners_title',
+        'sponsors_text',
+        'sponsors_partnership_description',
+        # tickets section
+        'pre_register_text',
+        'tickets_title',
+        'tickets_description',
+        # location section
+        'location_title',
+        'location_place',
+        # fields from Page model
+        'slug',
+        'url_path',
+        'live',
+        'speakers_title',
+        'schedule_title'
+    )
 
     required_languages = ("bg", "en")
 
